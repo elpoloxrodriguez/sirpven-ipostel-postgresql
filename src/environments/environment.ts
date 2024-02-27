1d2be7adf2cb0cb95a1b1106cf89c4ec88ab2b11
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2024-02-27 19:16:11 -0400

    OPP VE PAGOS DE SUB - OPP CAMBIA CLAVE DE SUB II

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   ID: 'ID-001',
   apiUrl: 'https://localhost',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
-  buildDateTime: 'Tue Feb 27 2024 19:06:35 GMT-0400 (Venezuela Time)',
+  buildDateTime: 'Tue Feb 27 2024 19:15:10 GMT-0400 (Venezuela Time)',
 };
 
 
