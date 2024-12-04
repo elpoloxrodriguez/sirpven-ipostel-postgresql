e958ee1d3e0b3b78459f4a9992bacdb036989661
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2024-12-04 05:31:00 -0400

    build modificaciones multiples

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
   ID: 'ID-001',
   apiUrl: 'https://sirp.ipostel.gob.ve',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
-  buildDateTime: 'Wed Jul 03 2024 20:08:16 GMT-0400 (Venezuela Time)',
+  buildDateTime: 'Wed Dec 04 2024 05:29:57 GMT-0400 (Venezuela Time)',
 };
